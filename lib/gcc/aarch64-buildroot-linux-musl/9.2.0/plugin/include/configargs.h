d5823a45c092d34cca8ac8fb70aed375d73e6ee8
ktommy91 <kemenesi.tamas@gmail.com>
2019-11-26 19:07:01 +0100

    First commit

---
+++ lib/gcc/aarch64-buildroot-linux-musl/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/tamas/Desktop/buildroot/output/host --sysconfdir=/home/tamas/Desktop/buildroot/output/host/etc --enable-static --target=aarch64-buildroot-linux-musl --with-sysroot=/home/tamas/Desktop/buildroot/output/host/aarch64-buildroot-linux-musl/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/tamas/Desktop/buildroot/output/host --with-mpc=/home/tamas/Desktop/buildroot/output/host --with-mpfr=/home/tamas/Desktop/buildroot/output/host --with-pkgversion='Buildroot 2019.11-rc1-01602-g3ab1810b65-dirty' --with-bugurl=http://bugs.buildroot.net/ --disable-libmpx --disable-libquadmath --disable-libsanitizer --enable-tls --enable-plugins --enable-lto --enable-threads --with-isl=/home/tamas/Desktop/buildroot/output/host --with-abi=lp64 --with-cpu=cortex-a75.cortex-a55 --enable-languages=c --with-build-time-tools=/home/tamas/Desktop/buildroot/output/host/aarch64-buildroot-linux-musl/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a75.cortex-a55" } };
